a7c987c5ed59d3319068eda5bb222f04e38a73a0
IreneuszKopiec <115220213+IreneuszKopiec@users.noreply.github.com>
2022-11-25 12:51:29 +0100

    lab4 i lab5

--- zapis.sql
+++ zapis.sql
@@ -42,3 +42,67 @@ select nazwa from postac where nazwa regexp '[a]';
 #lab4 zadanie 3b
 update statek set max_ladownosc = max_ladownosc*0.7 where data_wodowania >= '2000-01=01' AND data_wodowania <='3000-01-01';
 select *from statek;
+
+#lab4 zadanie 4a
+SELECT * FROM postac;
+#ALTER TABLE ...->dodanie wartosci 'waz' w kolumnie rodzaj w tabeli postac
+DESC postac;
+ALTER TABLE postac MODIFY rodzaj enum('wiking','ptak','syrena','kobieta','waz');
+show create table postac;
+INSERT INTO postac values ('12345678111',20,'Loko','waz','2000-01-01',10,null,null);
+
+#lab4 zadanie 4b
+show create table postac;
+#opcja 1 -like (przeniesie klucz glowny) - tylko struktura
+create table marynarz like postac;
+#dodanie danych
+insert into marynarz select * from postac;
+#opcja 2-  create as select (struktura + dane (bez klucza)
+create table marynarz select * from postac;
+
+
+#lab 4 zadanie 4c
+show tables;
+show create table marynarz;
+desc statek;
+ALTER TABLE marynarz ADD FOREIGN KEY(statek) references statek(nazwa_statku);
+show create table postac;
+
+#lab 4 zadanie 5a
+UPDATE marynarz SET statek=null;
+SELECT * FROM marynarz;
+
+#lab 4 zadanie 5b
+DELETE FROM marynarz WHERE id_postaci=12;
+
+#lab 4 zadanie 5c
+DELETE FROM statek;
+
+#lab 4 zadanie 5d
+alter table marynarz DROP foreign key `postac_ibfk_1`
+
+
+
+#lab5 zadanie 1.1
+create table kreatura select * from wikingowie.kreatura;
+create table zasob select * from wikingowie.zasob;
+create table ekwipunek select * from wikingowie.ekwipunek;
+
+#lab5 zadanie 1.2
+SELECT * FROM zasob;
+
+#lab5 zadanie 1.3
+SELECT * FROM zasob where rodzaj='jedzenie';
+
+#lab5 zadanie 1.4
+SELECT idZasobu,ilosc FROM zasob where idZasobu in(1,3,5);
+
+#lab5 zadanie 2.1
+SELECT * FROM kreatura WHERE rodzaj!='wiedzma' AND udzwig>=50;
+
+#lab5 zadanie 2.2
+SELECT * FROM zasob where waga>=2 AND waga<=5;
+
+#lab5 zadanie 2.3
+SELECT * FROM kreatura;
+SELECT * FROM kreatura WHERE nazwa LIKE '%or%' AND (udzwig>=30 AND udzwig<=70);
