c6534332f386fe7d10ec39a481da508d7ea44e54
IreneuszKopiec <115220213+IreneuszKopiec@users.noreply.github.com>
2022-12-02 12:51:11 +0100

    +lab 5 i 6

--- zapis.sql
+++ zapis.sql
@@ -106,3 +106,112 @@ SELECT * FROM zasob where waga>=2 AND waga<=5;
 #lab5 zadanie 2.3
 SELECT * FROM kreatura;
 SELECT * FROM kreatura WHERE nazwa LIKE '%or%' AND (udzwig>=30 AND udzwig<=70);
+
+#lab5 zadanie 3.1
+select dataPozyskania, 
+year(dataPozyskania),
+ month(dataPozyskania),
+ day(dataPozyskania) from wikingowie.zasob ;
+select * from wikingowie.zasob WHERE month(dataPozyskania) in (7,8);
+
+#lab5 zadanie 3.2
+SELECT * from wikingowie.zasob where rodzaj is not null ORDER BY waga ASC;
+
+#lab5 zadanie 3.3
+select * from wikingowie.kreatura order by dataur limit 5;
+
+#lab5 zadanie 4.1
+SELECT DISTINCT rodzaj from wikingowie.zasob;
+select distinct nazwa, dataPozyskania from wikingowie.zasob;
+select distinct(rodzaj) from zasob;
+
+#lab5 zadanie 4.2
+SELECT CONCAT(nazwa,' - ',rodzaj) FROM wikingowie.kreatura WHERE rodzaj LIKE 'wi%';
+
+#lab5 zadanie 4.3
+SELECT nazwa, ilosc*waga, dataPozyskania FROM wikingowie.zasob
+ where year(dataPozyskania) between 2000 AND 2007;
+
+#lab5 zadanie 5.1
+SELECT nazwa, ilosc*waga*0.7 as waga_netto,
+ ilosc*waga*0.3 as odpadki from wikingowie.zasob where rodzaj='jedzenie';
+
+#lab5 zadanie 5.2
+SELECT * FROM wikingowie.zasob WHERE rodzaj is null;
+
+#lab5 zadanie 5.3
+SELECT DISTINCT nazwa, rodzaj FROM wikingowie.zasob 
+where nazwa LIKE 'Ba%' OR nazwa LIKE '%os' ORDER BY nazwa ASC;
+
+#lab6 zadanie 1.1
+SELECT AVG(waga) FROM wikingowie.kreatura WHERE rodzaj='wiking';
+
+#lab6 zadanie 1.2
+SELECT rodzaj, AVG(waga), COUNT(*) FROM wikingowie.kreatura 
+GROUP BY rodzaj;
+
+#lab6 zadanie 1.3
+SELECT AVG(2022-year(dataUr)) as wiek, rodzaj 
+FROM wikingowie.kreatura GROUP BY rodzaj;
+
+#lab6 zadanie 2.1
+SELECT nazwa, rodzaj, SUM(waga*ilosc) 
+FROM wikingowie.zasob GROUP BY nazwa, rodzaj 
+with rollup order by rodzaj;
+
+# jako ciekawostka
+select year(dataPozyskania) as rok , month(dataPozyskania) as miesiac
+, count(*) from wikingowie.zasob group by rok, miesiac with rollup;
+
+#lab6 zadanie 2.2
+#having filtr dla kolumn agregowanych po agregacji
+select rodzaj, count(nazwa) from wikingowie.kreatura GROUP BY rodzaj
+HaVING count(nazwa)>1;
+
+SELECT nazwa, AVG(waga) 
+FROM wikingowie.zasob WHERE ilosc>=4 GROUP BY nazwa 
+HAVING AVG(waga)>10;
+
+SELECT * FROM wikingowie.zasob order by waga;
+#lub ilosc jako agregacja dla nazwy
+SELECT nazwa, AVG(waga) 
+FROM wikingowie.zasob GROUP BY nazwa 
+HAVING AVG(waga)>10 AND sum(ilosc)>=4 ;
+
+SELECT nazwa, AVG(waga),sum(ilosc) 
+FROM wikingowie.zasob GROUP BY nazwa;
+
+#lab6 zadanie 2.3
+SELECT rodzaj, COUNT(nazwa)FROM wikingowie.zasob
+WHERE ilosc>1 GROUP BY rodzaj;
+
+SELECT rodzaj, COUNT(nazwa)FROM wikingowie.zasob 
+GROUP BY rodzaj HAVING SUM(ilosc)>1;
+
+#lab6 zadanie 3.1
+SELECT *
+FROM wikingowie.kreatura, wikingowie.ekwipunek
+WHERE wikingowie.kreatura.idKreatury=wikingowie.ekwipunek.idKreatury;
+#równoważne
+SELECT k.nazwa, e.ilosc
+FROM wikingowie.kreatura k inner join wikingowie.ekwipunek e
+ON wikingowie.k.idKreatury=wikingowie.e.idKreatury;
+
+#lab6 zadanie 3.2
+SELECT k.nazwa, e.ilosc, z.nazwa
+FROM wikingowie.kreatura k inner join wikingowie.ekwipunek e 
+inner join wikingowie.zasob  z ON 
+wikingowie.z.idZasobu=wikingowie.e.idZasobu ON
+wikingowie.k.idKreatury=wikingowie.e.idKreatury;
+
+#lab6 zadanie 3.3
+SELECT * FROM wikingowie.kreatura k  left join wikingowie.ekwipunek e ON 
+wikingowie.k.idKreatury=wikingowie.e.idKreatury 
+WHERE wikingowie.e.idEkwipunku is null;
+
+#podzadanie
+select idKreatury from wikingowie.kreatura where idKreatury not in (
+select distinct idKreatury from wikingowie.ekwipunek
+ where idKreatury);
+
+#PD 4.1 4.2
